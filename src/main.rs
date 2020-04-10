636c09fda031896d51ec25516aa91229f3b4bb36
A. Bram Neijt <bram@neijt.nl>
2020-04-10 20:57:38 +0200

    Ugly method of running code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_files::NamedFile;
+use actix_web::client::Client;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use serde::Serialize;
 use std::collections::HashMap;
 use std::env;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Mutex;
 
 #[derive(Clone, Serialize)]
@@ -12,11 +14,10 @@ struct Tingle {
     result: String,
 }
 
-
 async fn index(
     req: HttpRequest,
     state_data: web::Data<Mutex<HashMap<String, Tingle>>>,
-) -> impl Responder  {
+) -> impl Responder {
     let host = format!("{}", req.peer_addr().unwrap().ip());
     let mut state = state_data.lock().unwrap();
     state.insert(
@@ -45,7 +46,7 @@ async fn touch(
     state.insert(
         party.clone(),
         Tingle {
-            action: "touched".into(),
+            action: "touch".into(),
             host: party.clone(),
             result: "nothing yet".into(),
         },
@@ -53,21 +54,68 @@ async fn touch(
     HttpResponse::Ok().json("rocking")
 }
 
+async fn run(
+    action_idx: web::Data<AtomicUsize>,
+    state_data: web::Data<Mutex<HashMap<String, Tingle>>>,
+) -> impl Responder {
+    let current_action_idx = action_idx.fetch_add(1, Ordering::SeqCst);
+    println!("Acting on {}", current_action_idx);
+    let mut state = state_data.lock().unwrap();
+    let tingle_vec: Vec<Tingle> = state.iter().map(|(_, tingle)| tingle.clone()).collect();
+    if tingle_vec.len() > 0 {
+        let client = Client::default();
+        let tingle: Tingle = (*tingle_vec
+            .get(current_action_idx % tingle_vec.len())
+            .unwrap())
+        .clone();
+        if tingle.action == "touch" {
+            let host: String = tingle.host.clone();
+            let response = client.get(format!("http://{}", host)).send().await;
 
+            match response {
+                Ok(result) => state.insert(
+                    tingle.host.clone(),
+                    Tingle {
+                        result: result
+                            .status()
+                            .canonical_reason()
+                            .unwrap_or("an unknown but good feeling")
+                            .to_string(),
+                        ..tingle
+                    },
+                ),
+                Err(_) => state.insert(
+                    tingle.host.clone(),
+                    Tingle {
+                        result: String::from("a feeling of rejection"),
+                        ..tingle
+                    },
+                ),
+            };
+            HttpResponse::Ok().json("ok")
+        } else {
+            HttpResponse::Ok().json("fail")
+        }
+    } else {
+        HttpResponse::Ok().json("nop")
+    }
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-
     let address = "0.0.0.0";
     let port = env::var("PORT").unwrap_or(String::from("8080"));
 
     let state: HashMap<String, Tingle> = HashMap::new();
     let state_data = web::Data::new(Mutex::new(state));
+    let action_idx = web::Data::new(AtomicUsize::new(0usize));
 
     HttpServer::new(move || {
         App::new()
             .app_data(state_data.clone())
+            .app_data(action_idx.clone())
             .route("/", web::get().to(index))
+            .route("/run", web::get().to(run))
             .route("/state", web::get().to(get_state))
             .route("/touch/{party}", web::get().to(touch))
     })
